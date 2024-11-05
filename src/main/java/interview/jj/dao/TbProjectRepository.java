3aef516d0d8c7b5c5055d6632b7d4b4da414b02b
Jan Jaroš <jan.jaros123@gmail.com>
2024-11-05 12:07:10 +0100

    remove needless @Repository annotations

--- src/main/java/interview/jj/dao/TbProjectRepository.java
+++ src/main/java/interview/jj/dao/TbProjectRepository.java
@@ -3,13 +3,11 @@ package interview.jj.dao;
 import interview.jj.entity.TbProjectEntity;
 import interview.jj.entity.TbUserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
-@Repository
 public interface TbProjectRepository extends JpaRepository<TbProjectEntity, Long> {
 
     Optional<TbProjectEntity> findByIdAndUser(UUID id, TbUserEntity user);
--- src/main/java/interview/jj/dao/TbUserRepository.java
+++ src/main/java/interview/jj/dao/TbUserRepository.java
@@ -2,11 +2,9 @@ package interview.jj.dao;
 
 import interview.jj.entity.TbUserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
-@Repository
 public interface TbUserRepository extends JpaRepository<TbUserEntity, Long> {
     Optional<TbUserEntity> findByEmail(String email);
 
